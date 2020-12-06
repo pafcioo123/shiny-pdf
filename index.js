a5ac433807d6ff196700d65f6b9728f3f2fb0fcb
pafcioo123 <l.kubiak2222@gmail.com>
2020-12-06 23:59:01 +0100

    get html from req body

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-var html_to_pdf = require('html-pdf-node');
-var fs = require('fs')
+const html_to_pdf = require('html-pdf-node');
+const fs = require('fs')
+const bodyParser = require('body-parser');
 // let options = { format: 'A4' };
 // var content = fs.readFileSync('Output.html', 'utf8');
 // // let file = { content: "<h1>Welcome to html-pdf-node</h1>" };
@@ -16,22 +17,23 @@ var fs = require('fs')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use(express.json());
+// app.use(express.raw());
+// app.use(express.urlencoded({ extended: false }))
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/pdf/', (req, res) => {
+app.get('/pdf/', bodyParser.json(),(req, res) => {
 
   let options = { format: 'A4' };
-  var content = req.body || fs.readFileSync('Output.html', 'utf8');
+  console.log("req",req)
+  console.log("req body",req.body)
+  const content = req.body.html || fs.readFileSync('Output.html', 'utf8');
   // var content = "<p>xd</p>";
   let file = { content: content };
   html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
-    // const download = Buffer.from(pdfBuffer.toString('utf-8'), 'base64');
-  // console.log("PDF Buffer:-", pdfBuffer);
-  // fs.writeFileSync('some.pdf', pdfBuffer);
-  //   res.set({"Content-Disposition":"attachment; filename=test.pdf"})
-  //   res.setHeader('Content-type', 'application/pdf');
     res.contentType("application/pdf");
   res.send(pdfBuffer)
 });
