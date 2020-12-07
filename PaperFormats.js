f786d08f03703cb047c98203449efab83676539c
pafcioo123 <l.kubiak2222@gmail.com>
2020-12-07 21:38:33 +0100

    uncompileable!!!!

---
+++ PaperFormats.js
@@ -0,0 +1,24 @@
+const A_Formats = [
+  "A0",
+  "A1",
+  "A2",
+  "A3",
+  "A4",
+  "A5",
+  "A6",
+  "A7",
+  "A8",
+  "A9",
+  "A10",
+  "A11",
+  "A12",
+  "A12",
+  "A13",
+  "2A0",
+  "4A0",
+  "A0+",
+  "A1+",
+  "A3+"
+];
+const PaperFormats = [].concat(A_Formats);
+module.exports = PaperFormats;
--- index.js
+++ index.js
@@ -1,28 +1,77 @@
-const express = require('express')
-const html_to_pdf = require('html-pdf-node');
-const fs = require('fs')
-const bodyParser = require('body-parser');
+const express = require("express");
+const html_to_pdf = require("html-pdf-node");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const yup = require("yup");
+const PaperFormats = require("./PaperFormats");
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+const pdfGeneratorSchema = yup.object().shape({
+  content: yup.string().required(),
+  options: yup.object().shape({
+    scale: yup
+      .number()
+      .min(0.1)
+      .max(2),
+    landscape: yup.boolean(),
+    format: yup.string().oneOf(PaperFormats),
+    margin: yup.object().shape({
+      top: yup.string(),
+      right: yup.string(),
+      bottom: yup.string(),
+      left: yup.string()
+    })
+  })
+});
 
-const app = express()
-const port = process.env.PORT || 3000
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/pdf/', bodyParser.json(),(req, res) => {
-  let options = { format: 'A4' };
-  console.log("req",req)
-  console.log("req body",req.body)
-  const content = req.body.html || fs.readFileSync('Output.html', 'utf8');
+app.get("/pdf/", bodyParser.json(), (req, res) => {
+  let options = { format: "A4" };
+  console.log("req", req);
+  console.log("req body", req.body);
+  const content = req.body.html || fs.readFileSync("Output.html", "utf8");
   let file = { content: content };
   html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
     res.contentType("application/pdf");
-  res.send(pdfBuffer)
+    res.send(pdfBuffer);
+  });
 });
 
-})
+app.get("/pdftest/", bodyParser.json(), (req, res) => {
+  let options = { format: "A4" };
+  console.log("req", req);
+  console.log("req body", req.body);
+  if (!!req.body)
+    res
+      .status(400)
+      .send("Missing json!!! Remember to set Content-Type:application/json!!!");
+  try {
+    const validatedValues = await async ()=> {pdfGeneratorSchema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true
+    })};
+    console.log("validated",validatedValues)
+    const file = { content: validatedValues.content};
+    const options= validatedValues.options
+    html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
+      res.contentType("application/pdf");
+      res.send(pdfBuffer);
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e.errors);
+  }
+  // const content = req.body.content || res.status(400).send("missing html code")
+  // let file = { content: content };
+  // html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
+  //   res.contentType("application/pdf");
+  //   res.send(pdfBuffer);
+  // });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
