6537b8c2059e8bdf902d3338eef4eef46b1d48d5
pafcioo123 <l.kubiak2222@gmail.com>
2021-01-04 15:36:33 +0100

    added html as file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 import PaperFormats from "./PaperFormats";
 import express from "express";
+import cors from "cors";
 const html_to_pdf = require("html-pdf-node");
 const fs = require("fs");
 const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const morgan = require("morgan");
 const yup = require("yup");
 // const PaperFormats = require("./PaperFormats");
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+const myLogger = function(req, res, next) {
+  console.log("LOGGED");
+  next();
+};
+// app.use(myLogger);
+app.use(fileUpload());
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -36,6 +54,22 @@ const check = async () => {
     stripUnknown: true
   });
 };
+
+app.post(
+  "/pdf2/",
+  (req, res) => {
+    let options = { format: "A4" };
+    // console.log("req", req);
+    // console.log("req body", req.body);
+    console.log(req.files,"files", fs.readFileSync("Output.html", "utf8"));
+    const content = req.files.html.data.toString() || fs.readFileSync("Output.html", "utf8");
+    let file = { content: content };
+    html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
+      res.contentType("application/pdf");
+      res.send(pdfBuffer);
+    });
+  }
+);
 app.get("/pdf/", bodyParser.json(), (req, res) => {
   let options = { format: "A4" };
   console.log("req", req);
@@ -66,17 +100,13 @@ app.get("/pdftest/", bodyParser.json(), async (req, res) => {
       console.log(e);
       res.status(400).send(e.errors);
     });
-  console.log(validatedValues)
-  const {options} = validatedValues
-  console.log('options',options)
+  console.log(validatedValues);
+  const { options } = validatedValues;
+  console.log("options", options);
   const content = validatedValues.content;
   let file = { content: content };
   html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
     res.contentType("application/pdf");
     res.send(pdfBuffer);
   });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
 });
